cdbe848cc0abb3d13c6276d2856b12b090bcbde0
Wes Gilleland <wes.gilleland@gmail.com>
2016-05-10 17:26:09 -0400

    Reformatted schema file

--- resources/mysql_schema.sql
+++ resources/mysql_schema.sql
@@ -1,27 +1,25 @@
 CREATE TABLE menu (
   id          INTEGER PRIMARY KEY AUTO_INCREMENT,
   time_of_day ENUM ('breakfast', 'lunch', 'dinner'),
-  `date`      DATE NOT NULL -- Are we gonna enforce date format here?
+  `date`      DATE NOT NULL
 );
 
 CREATE TABLE recipes (
   rec_id       INT PRIMARY KEY                         AUTO_INCREMENT,
   rec_name     VARCHAR(50) NOT NULL,
   instructions TEXT, -- character blob
-  -- not sure on these enums
   category     ENUM ('entree', 'appetizer', 'dessert') DEFAULT 'entree'
 );
 
 CREATE TABLE nutritional_fact (
   nfact_id   INT PRIMARY KEY AUTO_INCREMENT,
-  -- Assuming that we'll need precision in terms of 123.45
-  sodium     DECIMAL(6,2)         DEFAULT 0.00,
-  fat        DECIMAL(6,2)         DEFAULT 0.00,
-  calories   DECIMAL(6,2)         DEFAULT 0.00,
-  sugar      DECIMAL(6,2)         DEFAULT 0.00,
-  protein    DECIMAL(6,2)         DEFAULT 0.00,
-  food_group ENUM ('grain', 'meat', 'veggies') NOT NULL, -- Not sure what enums you need
-  amount     DECIMAL(6,2)         DEFAULT 0.00
+  sodium     DECIMAL(6, 2)   DEFAULT 0.00,
+  fat        DECIMAL(6, 2)   DEFAULT 0.00,
+  calories   DECIMAL(6, 2)   DEFAULT 0.00,
+  sugar      DECIMAL(6, 2)   DEFAULT 0.00,
+  protein    DECIMAL(6, 2)   DEFAULT 0.00,
+  food_group ENUM ('grain', 'meat', 'veggies') NOT NULL,
+  amount     DECIMAL(6, 2)   DEFAULT 0.00
 );
 
 CREATE TABLE food (
@@ -83,9 +81,11 @@ DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER on_nutritional_fact_delete
 BEFORE DELETE ON nutritional_fact
-  FOR EACH ROW
+FOR EACH ROW
   BEGIN
     -- When a nutritional fact is deleted, its id on the food entry should be set to null
-    UPDATE food SET fk_nfact_id=NULL WHERE fk_nfact_id=OLD.nfact_id;
+    UPDATE food
+    SET fk_nfact_id = NULL
+    WHERE fk_nfact_id = OLD.nfact_id;
   END $$
 DELIMITER ;
